9689ff4552d8f410bbf31f4dd0c9b7e8326b915c
Jesus Dominguez <j.dominguez@kodeline.com>
2021-08-08 08:39:36 +0200

    Rename data folder to input as name; Add DataSource service

--- src/app/Service/CsvImporter.php
+++ src/app/Service/CsvImporter.php
@@ -8,14 +8,14 @@ class CsvImporter {
     /**
      * @throws \Exception
      */
-    public function loadFile (string $file): array
+    public function loadFile (string $file, $separator = null): array
     {
         if (!file_exists($file)) {
             throw new \Exception(sprintf('File "%s" not found!', $file));
         }
 
-        return array_map(function ($content) {
-            return str_getcsv($content, ';');
+        return array_map(function ($content) use ($separator) {
+            return str_getcsv($content, $separator);
         }, file($file));
     }
 }
---
+++ src/app/Service/DataSource.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChallengeBestPlayer\Service;
+
+class DataSource {
+    protected const KEY_SOURCE_DIRECTORY = 'sourceDirectory';
+    protected const KEY_INPUT_FOLDER = 'inputFolder';
+    protected const KEY_CSV_SEPARATOR = 'csvSeparator';
+
+    protected CsvImporter $csvImporter;
+
+    protected array $options = [
+        self::KEY_SOURCE_DIRECTORY => __DIR__ . '/../../input/',
+        self::KEY_INPUT_FOLDER => 'challenge-csv-files',
+        self::KEY_CSV_SEPARATOR => ';',
+    ];
+
+    public function __construct()
+    {
+        $this->csvImporter = new CsvImporter();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function load(array $newOptions = []): array
+    {
+        $this->loadOptions($newOptions);
+
+        return $this->processFiles($this->getFilePaths());
+    }
+
+    /**
+     * @param array $newOptions
+     */
+    protected function loadOptions(array $newOptions = []): void
+    {
+        foreach (array_keys($this->options) as $optionKey) {
+            $this->options[$optionKey] = $newOptions[self::KEY_SOURCE_DIRECTORY] ?? $this->options[$optionKey];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSourceDirectoryPath(): string
+    {
+        return sprintf(
+            '%s%s',
+            $this->options[self::KEY_SOURCE_DIRECTORY],
+            $this->options[self::KEY_INPUT_FOLDER]
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function getFilePaths(): array
+    {
+        $directoryPath = $this->getSourceDirectoryPath();
+
+        if (!is_dir($directoryPath)) {
+            throw new \Exception(sprintf('Directory "%s" does not exists!', $directoryPath));
+        }
+
+        return array_filter(scandir($directoryPath), function ($item) use ($directoryPath) {
+            return
+                !is_dir(sprintf('%s/%s', $directoryPath, $item)) &&
+                pathinfo($item, PATHINFO_EXTENSION) === 'csv'
+                ;
+        });
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function processFiles(array $files): array
+    {
+        $directoryPath = $this->getSourceDirectoryPath();
+
+        return array_map(function ($file) use ($directoryPath) {
+            return $this->csvImporter->loadFile(
+                sprintf('%s/%s', $directoryPath, $file),
+                $this->options[self::KEY_CSV_SEPARATOR]
+            );
+        }, $files);
+    }
+}
