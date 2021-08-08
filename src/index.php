8c2d4590db036987fc61d6c4f149960a36650aa8
Jesus Dominguez <j.dominguez@kodeline.com>
2021-08-08 12:33:00 +0200

    Allow set different input folders from command line

--- src/index.php
+++ src/index.php
@@ -4,5 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use ChallengeBestPlayer\App;
 
+$options = [
+    'inputFolder' => 'challenge-csv-files',
+];
+
+if (isset($argv[1]) && !empty($argv[1])) {
+    $options['inputFolder'] = $argv[1];
+}
+
 $app = new App();
-$app->execute();
+$app->execute($options);
