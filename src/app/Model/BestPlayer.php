a9b3cb3bfb04871694f0fa3c82eb85e4a08463da
Jesus Dominguez <j.dominguez@kodeline.com>
2021-08-08 13:49:10 +0200

    Add logic to find best player from source data

--- src/app/Model/BestPlayer.php
+++ src/app/Model/BestPlayer.php
@@ -4,9 +4,44 @@ declare(strict_types=1);
 
 namespace ChallengeBestPlayer\Model;
 
-class BestPlayer {
-    public function execute(string $dataFilesDirectory): void
-    {
+use ChallengeBestPlayer\Api\GamePlayerInterface;
 
+class BestPlayer {
+    protected array $players = [];
+
+    public function execute(array $gamePlayers): array
+    {
+        $this->populatePlayers($gamePlayers);
+
+        return $this->findBestPlayer();
+    }
+
+    protected function populatePlayers(array $players): void
+    {
+        foreach ($players as $player) {
+            /** @var GamePlayerInterface $player */
+            if (!array_key_exists($player->getNickName(), $this->players)) {
+                $this->players[$player->getNickName()] = [
+                    'name' => $player->getName(),
+                    'team' => $player->getTeam(),
+                    'points' => [],
+                    'total' => 0.0
+                ];
+            }
+
+            $playerPoints = $player->getPoints();
+            $this->players[$player->getNickName()]['points'][$player->getGame()] = $playerPoints;
+            $this->players[$player->getNickName()]['total'] += $playerPoints;
+        }
+    }
+
+    protected function findBestPlayer(): array
+    {
+        $clonePlayers = $this->players;
+        usort($clonePlayers, function ($playerA, $playerB) {
+            return $playerB['total'] - $playerA['total'];
+        });
+
+        return $clonePlayers[0];
     }
 }
